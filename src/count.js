8951e090f36197583a9134d03fe8042044a7831f
Sarah Baldwin <Sarah@Sarahs-MacBook-Pro-2.local>
2015-06-05 02:13:26 -0400

    Switch to tabs instead of spaces

--- src/count.js
+++ src/count.js
@@ -1,38 +1,38 @@
 var count = {
-  textarea: document.getElementById("textarea"),
-  word: document.getElementById("word"),
-  words: document.getElementById("words"),
-  char: document.getElementById("char"),
-  chars: document.getElementById("chars")
+	textarea: document.getElementById("textarea"),
+	word: document.getElementById("word"),
+	words: document.getElementById("words"),
+	char: document.getElementById("char"),
+	chars: document.getElementById("chars")
 }
 
 function process() {
-  var value = textarea.value;
-  update(count.char, count.chars, value.length);
-  update(count.word, count.words, word(value));
+	var value = textarea.value;
+	update(count.char, count.chars, value.length);
+	update(count.word, count.words, word(value));
 }
 
 function update(target, plural, length) {
-  target.textContent = length;
+	target.textContent = length;
 
-  if (length === 1) {
-    plural.classList.add("hidden");
-  } else {
-    plural.classList.remove("hidden");
-  }
+	if (length === 1) {
+		plural.classList.add("hidden");
+	} else {
+		plural.classList.remove("hidden");
+	}
 }
 
 function word(value) {
-  var newWord = false;
-  var count = 0;
+	var newWord = false;
+	var count = 0;
 
-  for (var i = 0; i < value.length; ++i) {
-    var word = newWord;
-    newWord = !/\s/.test(value.charAt(i));
-    if (!word && newWord) ++count;
-  }
+	for (var i = 0; i < value.length; ++i) {
+		var word = newWord;
+		newWord = !/\s/.test(value.charAt(i));
+		if (!word && newWord) ++count;
+	}
 
-  return count;
+	return count;
 }
 
 window.addEventListener("load", process);
